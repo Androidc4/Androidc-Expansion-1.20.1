5309796ac94dedece940984c66d1ddd31e37e7cc
ANDROIDC4\Vasin <vasinacek@seznam.cz>
2024-10-30 21:55:16 +0100

    Initial commit

---
+++ src/main/java/net/androidc4/androids_expansion/AndroidsExpansionClient.java
@@ -0,0 +1,10 @@
+package net.androidc4.androids_expansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AndroidsExpansionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/androidc4/androids_expansion/AndroidsExpansionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.androidc4.androids_expansion;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AndroidsExpansionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
